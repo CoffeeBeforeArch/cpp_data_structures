ce039c43377103717f7a4c4492efbf6be7541aa5
Nick <coffeebeforearch@gmail.com>
2019-03-29 15:21:11 -0400

    add directory for upcomming stdlib data structures video

---
+++ data_structures/standard_library/stdlib_data_structures.cpp
@@ -0,0 +1,24 @@
+// This program shows off some elementary data structures included in
+// the C++ standard library
+// By: Nick from CoffeeBeforeArch
+
+#include <forward_list>
+#include <stack>
+
+using namespace std;
+
+int main(){
+    // Create a singly linked list
+    forward_list<int> int_sll;
+
+    for(int i = 0; i < n; i++){
+        if(i % 2){
+            int_sll.push_front(i);
+        }else{
+            int_sll.insert_after
+        }
+    }
+
+    // Create a stack
+    stack<int> int_stack;
+}
