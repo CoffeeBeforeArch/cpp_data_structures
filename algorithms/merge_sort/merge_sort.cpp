e72190d991b97ede37f574e11c0654c9212166b1
Nick <CoffeeBeforeArch@gmail.com>
2019-04-01 02:47:38 -0400

    add upcomming merge sort

---
+++ algorithms/merge_sort/merge_sort.cpp
@@ -0,0 +1,16 @@
+// This program implements merge sort in C++
+// By: From CoffeeBeforeArch
+
+// Merges to sorted sub-lists
+void merge(){
+
+}
+
+// Recursive functionfor sorting the array
+void merge_sort(){
+
+}
+
+int main(){
+
+}
