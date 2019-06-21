67fadc66d4078a047b9057266eae6a4c705454aa
Nick <CoffeeBeforeArch@gmail.com>
2019-06-21 19:51:16 -0400

    Add total bfs example

--- algorithms/graph_algorithms/bfs/bfs.cpp
+++ algorithms/graph_algorithms/bfs/bfs.cpp
@@ -30,14 +30,86 @@ Graph::Graph(int v){
     V = v;
 
     // Create new adjacency list
-    adj = new list<int>[v]
+    adj = new list<int>[v];
 }
 
 // Implementation of method to add edges
-Graph::addEdge(int v, int w){
+void Graph::addEdge(int v, int w){
     adj[v].push_back(w);
 }
 
-int main(){
+// Perform BFS given a starting vertex
+void Graph::BFS(int s){
+    // Start with all vertices as not visited
+    bool *visited = new bool[V];
+    for(int i = 0; i < V; i++){
+        visited[i] = false;
+    }
 
+    // Create a queue for BFS
+    list<int> queue;
+
+    // Starting vertex marked as visited and added to queue
+    visited[s] = true;
+    queue.push_back(s);
+
+    // Continue until queue is empty
+    while(!queue.empty()){
+        // Get the front of the queue and remove it
+        s = queue.front();
+        queue.pop_front();
+
+        // Get all adjacent vertices from that vertex
+        cout << "Checking adjacent vertices for vertex " << s << endl;
+        for(auto i = adj[s].begin(); i != adj[s].end(); i++){
+            // We only care about nodes not visited yet
+            if(!visited[*i]){
+                // Mark as visited
+                cout << "Visit and enqueue " << *i << endl;
+                visited[*i] = true;
+                // Push back to check this vertex's vertices
+                queue.push_back(*i);
+            }
+        }
+    }
+
+}
+
+int main(){
+    // Create a new graph
+    Graph g(6);
+
+    // Create some edges to the vertices
+    // Connections for vertex 0
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+
+    // Connections for vertex 1
+    g.addEdge(1, 0);
+    g.addEdge(1, 3);
+    g.addEdge(1, 4);
+
+    // Connections for vertex 2
+    g.addEdge(2, 0);
+    g.addEdge(2, 4);
+
+    // Connections for vertex 3
+    g.addEdge(3, 1);
+    g.addEdge(3, 4);
+    g.addEdge(3, 5);
+
+    // Connections for vertex 4
+    g.addEdge(4, 1);
+    g.addEdge(4, 2);
+    g.addEdge(4, 3);
+    g.addEdge(4, 5);
+
+    // Connections for vertex 5
+    g.addEdge(5, 3);
+    g.addEdge(5, 4);
+
+    // Perform BFS and print result
+    g.BFS(2);
+    
+    return 0;
 }
